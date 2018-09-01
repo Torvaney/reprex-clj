b1c07b21af20883440867a0e45dcba0e1a3018c1
Torvaney <torvaney@protonmail.com>
2018-09-01 17:26:20 +0100

    Test multi-expressions

--- test/reprex/core_test.clj
+++ test/reprex/core_test.clj
@@ -11,3 +11,16 @@
     ;; Common fns
     ; (is (= "(require 'clojure.string)\n; => " (eval-and-capture (require 'clojure.string))))
     ))
+
+
+(deftest test-reprex
+  (testing "Any number of expressions are captured correctly"
+    ;; Maths
+    (is (= "(+ 1 1)\n; => 2"  (reprex (+ 1 1))))
+    (is (= "(* 5 5)\n; => 25" (reprex (* 5 5))))
+    (is (= "(- 5 2)\n; => 3"  (reprex (- 5 2))))
+    (is (= "(+ 1 1)\n; => 2\n\n(* 5 5)\n; => 25"
+           (reprex (+ 1 1) (* 5 5))))
+    (is (= "(+ 1 1)\n; => 2\n\n(* 5 5)\n; => 25\n\n(- 5 2)\n; => 3"
+           (reprex (+ 1 1) (* 5 5) (- 5 2))))
+    ))
