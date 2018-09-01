3f1f6064562979e7d3c06ca68ad40206f3d0cf30
Torvaney <torvaney@protonmail.com>
2018-09-01 17:32:01 +0100

    Add codecov

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject reprex "0.1.0-SNAPSHOT"
+  :description "Reproducible examples for clojure"
+  :url "https://github.com/Torvaney/reprex-clj"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]])
