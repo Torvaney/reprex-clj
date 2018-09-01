85e832ac8c58c4fb002380333ae45ccc9035250b
Torvaney <torvaney@protonmail.com>
2018-09-01 23:55:26 +0100

    Run reprex in clean ns
    Closes #4

--- src/reprex/core.clj
+++ src/reprex/core.clj
@@ -6,6 +6,7 @@
 (def prompt  "; => ")
 (def spacing "\n\n")
 
+;; Rendering
 
 (defn embed-markdown
   "Embed a string containing code into a markdown chunk."
@@ -21,6 +22,7 @@
   [spacing xs]
   (-> (clojure.string/join spacing xs) embed-markdown))
 
+;; Capturing & evaluating expressions
 
 (defmacro capture-expr
   "Capture an expression and its output."
@@ -41,12 +43,39 @@
       `(reverse ~evald))))
 
 
+;; Managing namespaces
+;; (from https://stackoverflow.com/questions/27343707/i-would-like-to-run-load-file-in-a-sandboxed-namespace-in-clojure)
+
+(defmacro with-ns
+  "Evaluates body in another namespace.  ns is either a namespace
+  object or a symbol.  This makes it possible to define functions in
+  namespaces other than the current one."
+  [ns & body]
+  `(binding [*ns* (the-ns ~ns)]
+     ~@(map (fn [form] `(eval '~form)) body)))
+
+
+(defmacro with-temp-ns
+  "Evaluates body in an anonymous namespace, which is then immediately
+  removed.  The temporary namespace will 'refer' clojure.core."
+  [& body]
+  `(try
+     (create-ns 'sym#)
+     (let [result# (with-ns 'sym#
+                     (clojure.core/refer-clojure)
+                     ~@body)]
+       result#)
+     (finally (remove-ns 'sym#))))
+
+;; Reprex
+
+
 (defmacro reprex
   "Create a reproducible example. With 0 arguments, reprex will attempt to read
    code from the clipboard, otherwise it will evaluate any expressions provided
    as arguments."
   ([]
-    (let [code (read-string (clip/slurp-clipboard))]
-      `(reprex ~code)))
-  ([& exprs]
-     `(->> (capture-exprs ~exprs) (render-captured spacing))))
+     (let [code (read-string (clip/slurp-clipboard))]
+       `(reprex ~code)))
+  ([& body]
+     `(with-temp-ns (->> (capture-exprs ~body) (render-captured spacing)))))
--- test/reprex/core_test.clj
+++ test/reprex/core_test.clj
@@ -15,7 +15,6 @@
 
 (deftest test-capture-exprs
   (testing "Any number of expressions are captured correctly"
-    ;; Maths
     (is (= '("(+ 1 1)\n; => 2")  (capture-exprs ((+ 1 1)))))
     (is (= '("(* 5 5)\n; => 25") (capture-exprs ((* 5 5)))))
     (is (= '("(- 5 2)\n; => 3")  (capture-exprs ((- 5 2)))))
@@ -29,5 +28,11 @@
   (testing "Reprex is created as expected"
     (is (= "``` clojure\n(+ 1 1)\n; => 2\n```\n\nCreated by [reprex](https://github.com/Torvaney/reprex-clj)"
            (reprex (+ 1 1)))
-        (= "``` clojure\n(+ 1 1)\n; => 2\n\n(* 5 5)\n; => 25\n```\n\nCreated by [reprex](https://github.com/Torvaney/reprex-clj)"
-           (reprex (+ 1 1) (* 5 5))))))
+    (is (= "``` clojure\n(+ 1 1)\n; => 2\n\n(* 5 5)\n; => 25\n```\n\nCreated by [reprex](https://github.com/Torvaney/reprex-clj)"
+           (reprex (+ 1 1) (* 5 5)))))))
+
+(def test-sym 1)
+
+(deftest test-reprex-clean-ns
+  (testing "Reprex runs in a 'clean' namespace"
+    (is (thrown? RuntimeException (reprex (inc test-sym))))))
