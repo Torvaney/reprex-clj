23065096bf50c12df98eafaebd065d1ef4378747
Torvaney <torvaney@protonmail.com>
2018-09-01 18:59:55 +0100

    Update tests

--- test/reprex/core_test.clj
+++ test/reprex/core_test.clj
@@ -2,25 +2,24 @@
   (:require [clojure.test :refer :all]
             [reprex.core :refer :all]))
 
-(deftest test-eval-and-capture
+(deftest test-capture-expr
   (testing "Single expressions are captured correctly"
     ;; Maths
-    (is (= "(+ 1 1)\n; => 2"  (eval-and-capture (+ 1 1))))
-    (is (= "(* 5 5)\n; => 25" (eval-and-capture (* 5 5))))
-    (is (= "(- 5 2)\n; => 3"  (eval-and-capture (- 5 2))))
+    (is (= "(+ 1 1)\n; => 2"  (capture-expr (+ 1 1))))
+    (is (= "(* 5 5)\n; => 25" (capture-expr (* 5 5))))
+    (is (= "(- 5 2)\n; => 3"  (capture-expr (- 5 2))))
     ;; Common fns
-    ; (is (= "(require 'clojure.string)\n; => " (eval-and-capture (require 'clojure.string))))
+    ; (is (= "(require 'clojure.string)\n; => " (capture-expr (require 'clojure.string))))
     ))
 
 
-(deftest test-reprex
+(deftest test-capture-exprs
   (testing "Any number of expressions are captured correctly"
     ;; Maths
-    (is (= "(+ 1 1)\n; => 2"  (reprex (+ 1 1))))
-    (is (= "(* 5 5)\n; => 25" (reprex (* 5 5))))
-    (is (= "(- 5 2)\n; => 3"  (reprex (- 5 2))))
-    (is (= "(+ 1 1)\n; => 2\n\n(* 5 5)\n; => 25"
-           (reprex (+ 1 1) (* 5 5))))
-    (is (= "(+ 1 1)\n; => 2\n\n(* 5 5)\n; => 25\n\n(- 5 2)\n; => 3"
-           (reprex (+ 1 1) (* 5 5) (- 5 2))))
-    ))
+    (is (= '("(+ 1 1)\n; => 2")  (capture-exprs ((+ 1 1)))))
+    (is (= '("(* 5 5)\n; => 25") (capture-exprs ((* 5 5)))))
+    (is (= '("(- 5 2)\n; => 3")  (capture-exprs ((- 5 2)))))
+    (is (= '("(+ 1 1)\n; => 2" "(* 5 5)\n; => 25")
+           (capture-exprs ((+ 1 1) (* 5 5)))))
+    (is (= '("(+ 1 1)\n; => 2" "(* 5 5)\n; => 25" "(- 5 2)\n; => 3")
+           (capture-exprs ((+ 1 1) (* 5 5) (- 5 2)))))))
