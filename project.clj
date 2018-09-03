2babc083c253252f2e1e064a9ce2c8c2f8748613
Torvaney <torvaney@protonmail.com>
2018-09-03 21:48:47 +0100

    Add LICENSE to project.clj
    [skip ci]

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject reprex "0.1.0-SNAPSHOT"
   :description "Reproducible examples for clojure"
+  :license {:url "https://github.com/Torvaney/reprex-clj"
+            :comments "see LICENSE"}
   :url "https://github.com/Torvaney/reprex-clj"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]])
